b21ab6f62bd3783bc8557d8afb7e3ac9c9743ae8
SmirnovDD <123432145@bk.ru>
2025-08-23 16:09:21 +0200

    initial commit

---
+++ Source/TEST/TEST.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TEST.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TEST, "TEST" );
+
+DEFINE_LOG_CATEGORY(LogTEST)
+ 
